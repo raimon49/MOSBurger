b387006c981fe5146da9bcd718a8e3e2729af3d1
raimon <raimon49@hotmail.com>
2017-08-21 19:09:17 +0900

    Create Xcode project files

---
+++ Sources/MOSBurger.h
@@ -0,0 +1,19 @@
+//
+//  MOSBurger.h
+//  MOSBurger
+//
+//  Created by raimon on 2017/08/21.
+//  Copyright © 2017年 raimon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MOSBurger.
+FOUNDATION_EXPORT double MOSBurgerVersionNumber;
+
+//! Project version string for MOSBurger.
+FOUNDATION_EXPORT const unsigned char MOSBurgerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MOSBurger/PublicHeader.h>
+
+
--- Tests/MOSBurgerTests/MOSBurgerTests.swift
+++ Tests/MOSBurgerTests/MOSBurgerTests.swift
@@ -1,11 +1,38 @@
+//
+//  MOSBurgerTests.swift
+//  MOSBurgerTests
+//
+//  Created by raimon on 2017/08/21.
+//  Copyright ﾂｩ 2017蟷ｴ raimon. All rights reserved.
+//
+
 import XCTest
 @testable import MOSBurger
 
 class MOSBurgerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssertEqual(MOSBurger().text, "Hello, World!")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 
